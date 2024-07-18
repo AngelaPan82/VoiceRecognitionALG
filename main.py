1e1abfc951d99b70d5060f534a543a9927e479f8
Angela <77923942+AngelaPan82@users.noreply.github.com>
2024-07-18 14:46:37 +0200

    adding logging messages

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ print("Hi speak to me!")
 #trying non-blocking recognition
 #Print is replaced with logging.error because it gives more information for each error and proveds a timestamp
 def callback(recognizer, audio):
+    logging.debug("it works?")
     def process_audio(recognizer, audio):
+        logging.debug("Audio recieved?")
         try:
             text = recognizer.recognize_google(audio)
             logging.info("You said: " + text)
@@ -25,6 +27,7 @@ def callback(recognizer, audio):
 
 def adjusting_noise_level(mic, recognizer):
     while True:
+        logging.debug("Adjusting noise level")
         recognizer.adjust_for_ambient_noise(mic, duration=0.1)
         time.sleep(1)
 
@@ -33,19 +36,22 @@ recognizer = sr.Recognizer()
 #r = sr.Recognizer()
 #starting a seperate thread for adjusting the noise level
 with sr.Microphone() as mic:
+    logging.info("Microphone is working")
     noise_thread = threading.Thread(target=adjusting_noise_level, args=(mic, recognizer))
     noise_thread.daemon=True
     noise_thread.start()
+    logging.info("Say something!")
 
 
 # Start listening in the background 
 stop_listening = recognizer.listen_in_background(mic, callback)
+logging.info("Listening in the background.")
 
 # This is the main loop
 try:
     while True:
+        logging.debug("Main loop is running. !!!!")
         time.sleep(0.1)
-
 except KeyboardInterrupt:
     # If keyboard interrupt occurs it stops the program and writes  the following message:
     logging.info("\nProgram stopped by user.")
