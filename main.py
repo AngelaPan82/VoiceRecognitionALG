053f4c8723c095d4ca8f778ff67b51af1794eba4
Angela <77923942+AngelaPan82@users.noreply.github.com>
2024-07-25 15:37:35 +0200

    Trying threading again

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import speech_recognition as sr
 import time
+import threading
 
 print("Hi speak to me!")
 
 #trying non-blocking recognition
 #Print is replaced with logging.error because it gives more information for each error and proveds a timestamp
 def callback(recognizer, audio):
+    print("Callback  triggered")
+    def process_audio():
+        print("process_audio triggered")
     try:
         text = recognizer.recognize_google(audio)
         print("You said: " + text)
@@ -14,21 +18,23 @@ def callback(recognizer, audio):
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-r = sr.Recognizer()  
+    threading.Thread(target=process_audio).start()
 
 recognizer = sr.Recognizer()
 
 #starting a seperate thread for adjusting the noise level
 with sr.Microphone() as mic:
     recognizer.adjust_for_ambient_noise(mic,duration=0.1)
+    print("Say something!")
     
     
 stop_listening = recognizer.listen_in_background(mic, callback)
+print("Listening...")
 
 
 try:
-    
     while True:
+        print("Main loop running")
         time.sleep(0.1)
 except KeyboardInterrupt:
     # If keyboard interrupt occurs it stops the program and writes  the following message:
